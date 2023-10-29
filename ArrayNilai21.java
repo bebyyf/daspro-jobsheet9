c78b09393c7bb1d25cf2d381e971377455523998
bebyyf <bebifindia@gmail.com>
2023-10-29 18:10:21 +0700

    Modifikasi percobaan 2

--- ArrayNilai21.java
+++ ArrayNilai21.java
@@ -3,10 +3,14 @@ public class ArrayNilai21 {
     public static void main(String[] args) {
         Scanner input21 = new Scanner (System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i=0; i< 10; i++){
+        for (int i=0; i < nilaiAkhir.length; i++){
             System.out.println("Masukkan nilai akhir ke-"+i+ ":");
             nilaiAkhir[i]= input21.nextInt();
-            System.out.println("Nilai akhir ke-"+i+ "adalah " +nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70){
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }else if (nilaiAkhir[i] <70){
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
         }
 
     }
